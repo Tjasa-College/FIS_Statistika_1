eb7b458564d035c33ad0a0f1f596b1749db6dfdd
Tjasa Zilavec <tjasa.zilavec@gmail.com>
2025-11-23 13:02:20 +0100

    naredila grafe. sredi naloge 2

--- naloga/naloga.R
+++ naloga/naloga.R
@@ -17,10 +17,6 @@ getwd()
 
 #1. Opisovanje in urejanje podatkov
 
-#c)na osnovi spletne strani, ki je navedena kot vir podatkov, natančno opredelite populacijo (stvarna, krajevna, časovna opredelitev)
-#statistično enoto in vaše 4 dodeljene statistične spremenljivke. 
-#posamezno statistično spremenljivko natančno opredelite (vrsta spremenljivke glede na tip izražanja in tip merjenja)
-
 #a) Uporabljen ukaz za prikaz  vseh spremenljivk in enot v dani datoteki
 data = read.csv("FavouriteIndianFood.csv", header = TRUE)
 
@@ -49,3 +45,92 @@ data_urejeni = data.frame(zaposlitveni_status, cepljenje_proti_COVIDu, visina, t
 # višina: številska, zvezna, razmernostna
 # teža: številska, zvezna, razmernostna
 
+#2. Analiza številskih spremenljivk
+
+# a)Izračunajte glavne opisne statistike (srednje vrednosti in mere variabilnosti) za obe
+# številski spremenljivki in jih prikažite (ustavite sliko rezultatov). Rezultate tudi vsebinsko
+# interpretirajte.
+library(psych)
+#višina:
+#srednje vrednosti: aritmeticna sredina, modus, mediana
+
+mean(visina)
+median(visina)
+quantile(visina, probs=c(0.25, 0.5, 0.75))
+describe(visina)
+#srednja višina udeležencev je okoli 165.69cm
+#mediana je 167.64cm, kar pomeni, da je 50% udeležencev višjih od 167.64cm, 50% udeležencev pa nižjih od 167.64cm
+# iz rezultatov lahko sklepamo, da je povprečna višina udeležencev 165.69cm, 50% udeležencev je manjših od 167.64cm, 
+# četrtina ljudi pa je celo manjših od 156.6cm. Standardna deviacija (sd) je 13.92, kar pomeni, da ima večina ljudi velikost v rangu
+# razponu 151.77cm (mean-sd) in 179.61cm (mean+sd). 
+# Koeficient asimetrije (skew) je -0.23 (<0) kar pomeni, da imamo asimetrijo v levo (rep na levi in vrh na desni strani).
+#Asimetrija v levo pomeni, da je večje število ljudi višjih od povprečja
+# Koeficient sploščenosti (kurtosis) pa je -0.15 (<0) kar pomeni, da je porazdelitev višine sploščena. 
+# Razpršenost podatkov okoli povprečja je večja.
+165.69+13.92
+165.69-13.92
+
+
+#teža:
+mean(teza)
+median(teza)
+quantile(teza, probs=c(0.25, 0.5, 0.75))
+describe(teza)
+# iz rezultatov lahko sklepamo, da je povprečna teža udeležencev 71.97368 kg, 50% udeležencev ima manj kot 70.1 kg, 
+# četrtina ljudi pa ima manj kot 60.0kg. Standardna deviacija (sd) je 18.94, kar pomeni, da ima večina ljudi velikost v rangu
+# razponu 53.03kg (mean-sd) in 90.91kg (mean+sd). 
+# Koeficient asimetrije (skew) je 1.39 (>0) kar pomeni, da imamo asimetrijo v desno (rep na desni in vrh na levi strani).
+# Asimetrija v desno pomeni, da je večje število ljudi lažjih od povprečja
+# Koeficient sploščenosti (kurtosis) pa je 5.17 (>0) kar pomeni, da je porazdelitev teže zelo koničasta. 
+# Razpršenost podatkov okoli povprečja je manjša. 
+# Ker je vrednost sploščenosti tako zelo visoka, predvidevam, da je v podatkh tudi kar nekaj mejnih vrednosti
+# (uporabniki niso napisali prave teže, ampak neko zelo visoko ali nizko številko, kar lahko privede do dolgih repov in koničaste porazdelitve)
+# vidimo tudi da je najmanjša teža 30kg in največja teža 163kg, kar lahko nakazuje na velika odstopanja in posledično tako velik
+# koeficient razpršenosti.
+71.97+18.94
+71.97-18.94
+
+#b)Številski spremenljivki predstavite tudi grafično s pomočjo histograma in okvirja z ročaji.
+# Na osnovi grafičnih prikazov opišite značilnosti posamezne spremenljivke.
+
+library(ggplot2)
+
+#višina:
+
+#histogram
+ggplot(data = data_urejeni) +
+  geom_histogram(mapping = aes(x=visina), bins = 20, color='black', fill='white') + 
+  xlab("Višina anketirancev") + 
+  ylab("Število anketirancev") + 
+  ggtitle("Histogram - razporeditev višine")
+#okvir z ročaji
+ggplot(data = data_urejeni) +
+  geom_boxplot(mapping = aes(x=visina)) +
+  xlab("Višina anketirancev") +
+  ggtitle("Okvir z ročaji - razporeditev višine")
+
+
+#teža:
+
+#histogram
+ggplot(data = data_urejeni) + 
+  geom_histogram(mapping = aes(x=teza), bins = 20, color = 'black', fill = 'white') +
+  xlab("Teža anketirancev") + 
+  ylab("Število anketirancev") + 
+  ggtitle("Histogram - razporeditev teže")
+#okvir z ročaji
+ggplot(data = data_urejeni) +
+  geom_boxplot(mapping=aes(x=teza)) +
+  xlab("Teza anketirancev") + 
+  ggtitle("Okvir z ročaji - razporeditev teže")
+
+#c) hipoteza
+#glede na dane podathe predvidevam da teža in višina nista povezani med seboj. 
+#V primeru teže imamo večino rezultatov na zelo majhnem intervalu, v primeru višine pa je le ta bolj razpršenA
+
+
+# d) Glede na vsebino spremenljivk določite odvisno (Y) in neodvisno (X) spremenljivko.
+# Nato narišite razsevni grafikon tako da je na x-osi spremenljivka X, na y-osi pa
+# spremenljivka Y. Ustrezno preimenujte naslova osi, grafikonu dodajte tudi glavni naslov.
+# Ali razsevni grafikon nakazuje na povezanost obravnavanih spremenljivk? Svoj odgovor
+# pojasnite.
